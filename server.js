921e71eea58ec42a06872535953eb383714715b4
Niraj Kumar Roy <nirajroy149@gmail.com>
2023-06-17 23:10:34 +0530

    fixed port error

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors());
 // for getting the data in post request
 app.use(express.urlencoded({ extended: true }));
 
-const PORT = process.env.port;
+const PORT = process.env.PORT || 5000;
 
 const DB_URL = process.env.DB_URL;
 
